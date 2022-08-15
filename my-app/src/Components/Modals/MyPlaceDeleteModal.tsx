b395d3fea8f494623a520655714f13efee557632
kol242 <kolinger315@gmail.com>
2022-08-15 21:30:32 +0200

    Added component
    Added MyPlaceDeleteModal

---
+++ my-app/src/Components/Modals/MyPlaceDeleteModal.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import '../../Common/Style/modal.css'
+
+const MyPlaceDeleteModal: React.FC<{ modalHandler: any, data: any }> = (props) => {
+  return (
+    <div className="modal-container">
+      <div className="modal-container__content">
+        <h1 id="content-title">Delete listing?</h1>
+        <div className="content-text">
+          <p>Are you sure you want to delete this listing? This action cannot be reversed.</p>
+        </div>
+        <div className="content-btns">
+          <button id="content-btns__btn" onClick={(event) => props.modalHandler(event)}>Cancel</button>
+          <button id="content-btns__btn">Delete</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default MyPlaceDeleteModal
--- my-app/src/Components/PlaceCard/PlaceView.tsx
+++ my-app/src/Components/PlaceCard/PlaceView.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useState } from 'react'
+import MyPlaceDeleteModal from '../Modals/MyPlaceDeleteModal'
 
 type Place = {
   title: string,
@@ -8,18 +9,28 @@ type Place = {
 }[]
 
 const PlaceView: React.FC<{ places: Place }> = (props) => {
+  const [modal, setModal] = useState(false);
+  const [modalData, setModalData] = useState({});
+
+  const modalHandler = (data: any) => {
+    setModalData(data)
+    !modal ? setModal(true) : setModal(false);
+  };
+
+
   return (
     <div className="place-container">
+      { modal && <MyPlaceDeleteModal modalHandler={modalHandler} data={modalData}/> }
         { props.places.map(place => (
             <div className="place-container__card">
-            <img src={place.imageUrl} alt="PlaceImg" />
-            <h1 id="place-container__card--title">{place.title}</h1>
-            <h3 id="place-container__card--subtitle">{place.location}</h3>
-            <h2 id="place-container__card--detail">{place.subtitle}</h2>
-            <div className="place-container__card--btns">
-                <button id="btns-edit">EDIT</button>
-                <button id="btns-delete">DELETE PLACE</button>
-            </div>
+              <img src={place.imageUrl} alt="PlaceImg" />
+              <h1 id="place-container__card--title">{place.title}</h1>
+              <h3 id="place-container__card--subtitle">{place.location}</h3>
+              <h2 id="place-container__card--detail">{place.subtitle}</h2>
+              <div className="place-container__card--btns">
+                  <button id="btns-edit">EDIT</button>
+                  <button onClick={() => modalHandler(place)}id="btns-delete">DELETE PLACE</button>
+              </div>
             </div>   
         )) }
     </div>
